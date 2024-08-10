177f5f7e554ce640f21390988c586b4a96bcb64e
iby3l <looqgabriel@gmail.com>
2024-08-10 00:00:57 -0300

    export

---
+++ lib/core/core.dart
@@ -0,0 +1,10 @@
+export 'base_http.dart';
+export 'base_injector.dart';
+export 'base_repository.dart';
+export 'base_usecase.dart';
+export 'error/base_failure.dart';
+export 'error/exceptions.dart';
+export 'infra/base_conector.dart';
+export 'infra/base_http_interface.dart';
+export 'infra/base_interceptor.dart';
+export 'result_state.dart';
